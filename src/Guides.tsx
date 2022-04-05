830e868aa3bca3bcebfaaa36b750e3815e8af5f3
usmangeek <usman@galaniprojects.de>
2022-04-05 20:20:15 +0200

    feat: added steps

--- src/Guides.tsx
+++ src/Guides.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { CustomDropdown } from './CustomDropdown'
+import { getPayerAccount } from './Utilts/backend-requests-helpers'
 
 const GuideContainer = styled.div`
   max-width: 1100px;
@@ -7,53 +9,209 @@ const GuideContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   flex-wrap: wrap;
-  margin-top: 100px;
+  margin-top: 50px;
   @media (max-width: 900px) {
     justify-content: center;
   }
 `
-const LinksContainer = styled.div`
-  margin-left: 20px;
-  margin-right: 20px;
-  display: flex;
-  flex-direction: column;
+const Steps = styled.span`
+  margin-top: 10px;
+  font-size: 14px;
+  line-height: 22px;
+  font-size: 14px;
+  margin-left: 10px;
 `
-const HeadingSpan = styled.span`
-  color: #43142b;
+const HeadTitle = styled.span`
   font-size: 24px;
   font-weight: 300;
-  letter-spacing: 0;
-  line-height: 26px;
-  height: 31px;
-  margin-bottom: 10px;
+  line-height: 37px;
 `
-const LinksSpan = styled.span`
+const InputContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  font-family: 'Overpass';
-
-  gap: 10px;
-  a {
-    color: #871e50;
-    font-family: 'Overpass';
-    font-size: 16px;
-    letter-spacing: 0;
-    line-height: 17px;
+  color: #43142b;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  letter-spacing: 0;
+  max-width: 550px;
+  box-sizing: border-box;
+  height: 60px;
+  border: 1px solid rgba(151, 151, 151, 0.5);
+  border-radius: 15px;
+  background-color: rgba(0, 0, 0, 0.1);
+  box-shadow: inset 2px 2px 6px 0 rgba(0, 0, 0, 0.3);
+  align-items: center;
+  justify-content: center;
+`
+const Input = styled.input`
+  height: 58px;
+  width: 90%;
+  color: #411329;
+  font-family: Overpass;
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 0;
+  line-height: 28px;
+  background-color: transparent;
+  border: none;
+  :focus {
+    outline: none;
   }
 `
+const ConnectBtn = styled.button`
+  display: flex;
+  color: white;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  letter-spacing: 0;
+  height: 60px;
+  border-radius: 15px;
+  background: radial-gradient(circle, #44142b 0%, #010000 100%);
+  box-shadow: -2px 8px 12px 0 rgba(0, 0, 0, 0.35);
+  font-size: 18px;
+  font-weight: 600;
+  letter-spacing: 0;
+  line-height: 28px;
+  text-transform: uppercase;
+  align-items: center;
+  justify-content: center;
+`
+const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #43142b;
+  letter-spacing: 0;
+  max-width: 550px;
+  width: 90%;
+
+  p {
+    font-size: 14px;
+    line-height: 22px;
+  }
+`
+const SpacerElement = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: #43142b;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  opacity: 0.4;
+`
+
+const MainText = () => {
+  return (
+    <LinksContainer>
+      <HeadTitle>web3name</HeadTitle>
+      <p>
+        Your web3name (w3n) represents your unique on-chain decentralized
+        identifier (DID), a string of letters and numbers that form the core of
+        your KILT digital identity.
+      </p>
+      <p>
+        web3name provides a unique personal and recognizable way to represent
+        yourself across the Polkadot ecosystem. Validators and collators can
+        customize a name that makes them easy to identify, and may link this
+        name to all their different addresses in the Polkadot ecosystem. This
+        name can also be used with DIDsign, adding an extra layer of
+        verification when you share digital files you have signed.
+      </p>
+      <p>
+        Normally, creating an on-chain DID and a web3name requires a deposit of
+        around 2 KILT Coins and a small transaction fee. Now, for a limited
+        time, you can create both your web3name and your on-chain DID for free.
+      </p>
+    </LinksContainer>
+  )
+}
+const StepOne = () => {
+  return (
+    <LinksContainer>
+      <HeadTitle>Getting started</HeadTitle>
+      <p>
+        In order to claim your name, you need a Sporran wallet and a KILT
+        on-chain DID. Here’s how to set up your Sporran. Then you can create
+        your free on-chain DID
+      </p>
+      <p>
+        Once you have these, you’re ready to go. Just follow the steps below.
+      </p>
+    </LinksContainer>
+  )
+}
+const StepTwo = () => {
+  return (
+    <LinksContainer>
+      <HeadTitle>Claim your name </HeadTitle>
+      <Steps>1.&nbsp;&nbsp; Open your Sporran extension</Steps>
+      <Steps> 2.&nbsp;&nbsp; Click “Create web3name”</Steps>
+      <Steps>
+        {' '}
+        3.&nbsp;&nbsp; Click and read the Terms. If you agree, check the box
+        “Use web3name Promo and accept Terms”. This will create a signed package
+        that is sent to the web3name promo server for payment and submission to
+        the blockchain.
+      </Steps>
+      <Steps>4.&nbsp;&nbsp; Click “Next Step”</Steps>
+      <Steps>5.&nbsp;&nbsp; Enter the name you wish to claim.</Steps>
+      <Steps>
+        6.&nbsp;&nbsp; When you have chosen an available name, click “Next”
+      </Steps>
+      <Steps>7.&nbsp;&nbsp; Enter your Sporran password and click “Sign </Steps>
+      <p>Congratulations, you now have your unique web3name!</p>
+    </LinksContainer>
+  )
+}
+const StepThree = () => {
+  return (
+    <LinksContainer>
+      <HeadTitle>Link your web3name to your account address</HeadTitle>
+      <p>
+        You may link your on-chain DID, with your new web3name, to any accounts
+        you have in the Polkadot ecosystem.{' '}
+      </p>
+      <p>
+        In this way, people with your Polkadot ecosystem account address will be
+        able to see your DID and your web3 name and, vice versa, those with your
+        web3name can see your public address. This is especially useful for
+        validators and collators, who can provide a personalized name that makes
+        them easily identifiable.
+      </p>
+      <p>To link your web3name to your ecosystem account address:</p>
+      <Steps>1.&nbsp;&nbsp; Open your Sporran extension</Steps>
+      <Steps> 2.&nbsp;&nbsp; Click “Manage on-chain DID” </Steps>
+      <Steps>
+        {' '}
+        3.&nbsp;&nbsp; Click the icon to the right of your DID to copy it
+      </Steps>
+      <Steps>4.&nbsp;&nbsp; Paste your DID into the field below:</Steps>
+      <InputContainer>
+        <Input />
+      </InputContainer>
+      <Steps>5.&nbsp;&nbsp; Click “Connect to wallet” </Steps>
+      <ConnectBtn>Connect To Wallet</ConnectBtn>
+      <p>
+        This opens up pop-ups to request access to all your Polkadot-enabled
+        extensions, including Sporran)
+      </p>
+      <Steps>6.&nbsp;&nbsp; Click “Allow access” on each wallet </Steps>
+      <Steps>
+        7.&nbsp;&nbsp; Click next to “Choose Account Name” in the dropdown list
+        and choose the account you wish to link to your web3name{' '}
+      </Steps>
+      <CustomDropdown />
+    </LinksContainer>
+  )
+}
+
 export const Guides = () => {
   return (
     <GuideContainer>
-      <LinksContainer>
-        <HeadingSpan>Index</HeadingSpan>
-        <LinksSpan>
-          <a href=" ">01. Download Sporran</a>
-          <a href=" ">02. Download Sporran</a>
-          <a href=" ">03. Download Sporran</a>
-          <a href=" ">04. Download Sporran</a>
-          <a href=" ">05. Download Sporran</a>
-        </LinksSpan>
-      </LinksContainer>
+      <MainText />
+      <SpacerElement />
+      <StepOne />
+      <SpacerElement />
+      <StepTwo />
+      <SpacerElement />
+      <StepThree />
     </GuideContainer>
   )
 }
