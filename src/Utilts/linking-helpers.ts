c1d5c13f7b24e8f4a280d2653939610c25a02506
usmangeek <usman@galaniprojects.de>
2022-04-06 15:09:18 +0200

    feat: account linking

--- src/Utilts/linking-helpers.ts
+++ src/Utilts/linking-helpers.ts
@@ -4,7 +4,13 @@ import type { MultiSignature, AccountId } from '@polkadot/types/interfaces'
 import { KeypairType } from '@polkadot/util-crypto/types'
 import type { AnyNumber } from '@polkadot/types/types'
 import type { HexString } from '@polkadot/util/types'
-import { signatureVerify } from '@polkadot/util-crypto'
+import { encodeAddress, signatureVerify } from '@polkadot/util-crypto'
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromAddress,
+} from '@polkadot/extension-dapp'
+import { getPayerAccount, submitDidRequest } from './backend-requests-helpers'
 
 type AccountAddress = string
 type SignatureType = MultiSignature['type']
@@ -20,6 +26,65 @@ export const connect = async () => {
   return await ApiPromise.create({
     provider,
   })
+}
+
+const getApi = async (): Promise<ApiPromise> => {
+  return await connect()
+}
+export const getFilteredAccounts = async () => {
+  await web3Enable('web3name-promo by BTE')
+  const allAccounts = await web3Accounts()
+  const api = await getApi()
+  const genesisHash = api.genesisHash.toHex()
+  const filteredAccounts = allAccounts.filter(
+    (account) =>
+      !account.meta.genesisHash || account.meta.genesisHash === genesisHash
+  )
+  return filteredAccounts
+}
+export const linkDidWithAccount = async (
+  account: any,
+  did: string
+): Promise<HexString> => {
+  const api = await getApi()
+  const ss58Prefix = api.registry.chainSS58
+  const encodedAccountAddresses = encodeAddress(account.address, ss58Prefix)
+  const injector = await web3FromAddress(encodedAccountAddresses)
+  const didID = did.split(':').pop()
+  if (!didID) throw Error('DID Address Undefined')
+  const extrinsic = await authorizeLinkWithAccount(
+    api,
+    encodedAccountAddresses,
+    didID,
+    async (payload, address) => {
+      if (!injector.signer.signRaw)
+        throw Error("Extension doesn't support signRaw")
+      const result = await injector.signer.signRaw({
+        data: payload,
+        address,
+        type: 'bytes',
+      })
+      return result.signature
+    }
+  )
+  api.disconnect()
+  const payerAccountAddress = await getPayerAccount()
+  const signedOutputFromSporran =
+    await window.kilt.sporran.signExtrinsicWithDid(
+      extrinsic.toHex(),
+      payerAccountAddress
+    )
+
+  const submittableExtrinsic = api.createType(
+    'Extrinsic',
+    signedOutputFromSporran.signed
+  )
+
+  const tx_hash = await submitDidRequest(
+    submittableExtrinsic.args[0].toHex(),
+    submittableExtrinsic.args[1].toHex()
+  )
+  return tx_hash
 }
 function getMultiSignatureTypeFromKeypairType(
   keypairType: KeypairType
@@ -70,4 +135,8 @@ export async function authorizeLinkWithAccount(
     signature,
     sigType
   )
+}
+export const isKiltDid = (searchedText: string): boolean => {
+  const kiltKeyword = searchedText.split(':').slice(1, -1)
+  return kiltKeyword.includes('kilt') && kiltKeyword.length === 1
 }
