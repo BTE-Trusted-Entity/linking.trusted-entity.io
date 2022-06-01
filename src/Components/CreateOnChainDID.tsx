08dbbb692517f3251ec01ed0fa5c7aa1f3142f64
Marton Csernai <marton@kilt.io>
2022-06-01 20:02:41 +0200

    fill out CreateOnChainDID box

--- src/Components/CreateOnChainDID.tsx
+++ src/Components/CreateOnChainDID.tsx
@@ -48,6 +48,16 @@ const Content = styled.div`
   padding: 0 1.25rem 2.25rem 1.25rem;
 `
 
+const MainText = styled.p`
+  margin-top: 0;
+  line-height: 150%;
+`
+const LinkToGuide = styled.a`
+  display: block;
+  margin: 0.5rem 0.5rem;
+  color: ${colors.white};
+  text-decoration: underline;
+`
 const CollapseBtn = styled.button`
   background: url(${CollapseIcon}) no-repeat center/auto;
   position: absolute;
@@ -77,13 +87,39 @@ export const CreateOnChainDID = () => {
 
       {expanded && (
         <Content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
+          <MainText>
+            Your decentralized Identifier (DID) is a unique set of numbers and
+            letters that represents your identity, like a digital fingerprint.
+            When you upgrade to an on-chain DID, you can link multiple things to
+            your DID including:
+          </MainText>
+          <ul>
+            <li>Your unique web3name </li>
+            <li>
+              As many of your Polkadot ecosystem (and soon, Ethereum) addresses
+              that you wish
+            </li>
+            <li>
+              Any credentials you want to make public, such as social media
+              handles, GitHub and email addresses
+            </li>
+            <li>Communication endpoints, e.g., your website</li>
+          </ul>
+          <p>
+            Upgrading to an on-chain DID requires a deposit of 2 KILT and a
+            small transaction fee (around 0.0045 KILT).
+          </p>
+          <ol>
+            <li>Open your Sporran extension</li>
+            <li>Click “Upgrade to on-chain DID”</li>
+          </ol>
+          <LinkToGuide
+            href="https://www.trusted-entity.io/assets/pdf/Upgrading-to-on-chain-DID.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Guide to upgrading your DID (PDF)
+          </LinkToGuide>
           <CollapseBtn
             onClick={() => setExpanded(false)}
             aria-label="Collapse content"
