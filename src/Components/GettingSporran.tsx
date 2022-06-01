66c91675e4804fb5d18655c8de81215db8bab498
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2022-06-01 16:57:17 +0200

    apply same style to all cards

--- src/Components/GettingSporran.tsx
+++ src/Components/GettingSporran.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import styled from 'styled-components'
 import ExpandIcon from '../ImageAssets/Open.svg'
 import CollapseIcon from '../ImageAssets/Close.svg'
@@ -6,89 +6,89 @@ import CollapseIcon from '../ImageAssets/Close.svg'
 import { colors } from '../Theme/colors'
 
 interface Style {
-  expand: boolean
+  expanded: boolean
 }
+
 const Container = styled.section`
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   max-width: 1182px;
-  width: 90%;
+  width: 82%;
   justify-content: flex-start;
   align-items: flex-start;
-  border-radius: 12px;
-  box-shadow: 0 6px 8px 0 rgba(0, 0, 0, 0.25);
+  border-radius: 0.75rem;
+  box-shadow: 0 0.375rem 0.5rem 0 rgba(0, 0, 0, 0.25);
   background-color: ${colors.turquoise};
-  padding: 12px 20px 12px 20px;
-  height: ${(props: Style) => (props.expand ? '344px' : '40px')};
-  margin-bottom: 17px;
-  transition: height 0.5s ease-in-out;
+  margin-bottom: 1.25rem;
   position: relative;
-  pointer-events: ${(props: Style) => props.expand && 'auto'};
+
+  &[aria-expanded='false'] {
+    background: url(${ExpandIcon}) no-repeat right 0.5rem center,
+      ${colors.turquoise};
+  }
 `
+
 const Heading = styled.h1`
+  box-sizing: border-box;
   color: ${colors.white};
-  font-family: Overpass;
+  width: 100%;
+  margin: 0;
   font-size: 1.7rem;
   font-weight: 300;
-  letter-spacing: 0;
-  margin-top: 5px;
-  margin-bottom: 10px;
+  padding: 0.75rem 1.25rem;
+  cursor: ${({ expanded }: Style) => (expanded ? 'default' : 'pointer')};
+  border-radius: inherit;
 `
-const SporranGuide = styled.p`
+
+const Content = styled.div`
   color: ${colors.white};
-  font-family: Overpass;
-  font-size: 16px;
   letter-spacing: 0.1px;
-  line-height: 26px;
   word-break: normal;
+  padding: 0 1.25rem 2.25rem 1.25rem;
 `
-const ExtensionWrapper = styled.div`
-  display: flex;
-  max-width: 500px;
-  flex-wrap: wrap;
-  flex-grow: 1;
-`
-const Extension = styled.div`
-  height: 75.6px;
-  width: 250px;
-  box-shadow: 0 6px 8px 0 rgba(0, 0, 0, 0.25);
-`
-const ChromeExtension = styled(Extension)`
-  background-image: url;
-`
-const ExpandBtn = styled.button`
-  background: url(${ExpandIcon});
-  height: 30px;
-  width: 30px;
+
+const CollapseBtn = styled.button`
+  background: url(${CollapseIcon}) no-repeat center/auto;
   position: absolute;
-  right: 12px;
-  bottom: 12px;
-  background-repeat: no-repeat;
+  height: 1.5rem;
+  width: 1.5rem;
+  right: 0.5rem;
+  bottom: 1.25rem;
   border: none;
-  cursor: pointer;
-`
-const CollapseBtn = styled(ExpandBtn)`
-  background: url(${CollapseIcon});
-  background-repeat: no-repeat;
   cursor: pointer;
 `
 
 export const GettingSporran = () => {
-  const [expand, setExpand] = useState<boolean>(false)
+  const [expanded, setExpanded] = useState<boolean>(false)
+
+  const handleExpand = useCallback(() => {
+    if (expanded) {
+      return
+    }
+    setExpanded(true)
+  }, [expanded])
+
   return (
-    <Container expand={expand}>
-      <Heading>1. Get your Sporran wallet</Heading>
-      <SporranGuide>
-        The Sporran wallet is a browser extension that interacts with the KILT
-        blockchain, displaying KILT Coin balances and enabling signing and
-        sending transactions. The wallet also stores credentials, allowing you
-        to build a decentralized digital identity and control who sees your
-        data.
-      </SporranGuide>
-      {expand ? (
-        <CollapseBtn onClick={() => setExpand(false)} />
-      ) : (
-        <ExpandBtn onClick={() => setExpand(true)} />
+    <Container aria-expanded={expanded}>
+      <Heading onClick={handleExpand} expanded={expanded}>
+        1. Get your Sporran wallet
+      </Heading>
+
+      {expanded && (
+        <Content>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+          <CollapseBtn
+            onClick={() => setExpanded(false)}
+            aria-label="Collapse content"
+          />
+        </Content>
       )}
     </Container>
   )
