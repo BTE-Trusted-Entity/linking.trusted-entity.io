9b13e035f5cf542b1b3615572a53902969be9934
usmangeek <usman@galaniprojects.de>
2022-08-08 16:58:01 +0200

    SK-1159: add linking modal

---
+++ src/Components/Popup/Popup.module.css.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly modalContainer: string;
+  readonly modal: string;
+  readonly heading: string;
+  readonly modalText: string;
+  readonly loader: string;
+  readonly btn: string;
+  readonly status: string;
+  readonly btnDisabled: string;
+  readonly step: string;
+  readonly spinnerContainer: string;
+  readonly closeBtn: string;
+};
+export = styles;
