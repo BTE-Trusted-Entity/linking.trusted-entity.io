b790c5af48626857257e6b333bc38b28d765aac6
Muhammad Usman <94175349+usmangeek@users.noreply.github.com>
2022-08-09 14:27:42 +0200

    SK-1159: add linking modal (#16)
    * SK-1159: add linking modal
    
    * fix: update text for steps
    
    * fix: update linking step

--- src/Components/LinkingButton/LinkingButton.tsx
+++ src/Components/LinkingButton/LinkingButton.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { web3FromAddress } from '@polkadot/extension-dapp';
 import { SubmittableExtrinsic } from '@polkadot/api/types';
 
+import { encodeAddress } from '@polkadot/util-crypto';
+
 import styles from './LinkingButton.module.css';
 
 import {
+  authorizeLinkWithAccount,
+  getApi,
   InjectedAccount,
   isKiltDid,
-  linkDidWithAccount,
 } from '../../Utilts/linking-helpers';
+import { LinkingModal } from '../Popup/Popup';
 
 interface Wallet {
   linkingAccount?: InjectedAccount;
@@ -17,11 +21,64 @@ interface Wallet {
   payerAccount?: InjectedAccount;
 }
 
-export const LinkingButton = (props: Wallet) => {
-  const [error, setError] = useState<string | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+export type LinkingSteps = 1 | 2 | 3 | 4;
 
-  const [linking, setLinking] = useState(false);
+export const LinkingButton = (props: Wallet) => {
+  const [linkingStatus, setLinkingStatus] = useState<
+    'linking' | 'success' | 'error' | undefined
+  >();
+
+  const [linkingStep, setLinkingStep] = useState<LinkingSteps>(1);
+
+  const linkDidWithAccount = async (
+    linkingAccount: InjectedAccount,
+    payerAccount: InjectedAccount,
+    did: string,
+  ) => {
+    const api = await getApi();
+    const ss58Prefix = api.registry.chainSS58;
+    const payerAddress = payerAccount.address;
+    const encodedAccountAddresses = encodeAddress(
+      linkingAccount.address,
+      ss58Prefix,
+    );
+    const injector = await web3FromAddress(encodedAccountAddresses);
+    const didID = did.split(':').pop();
+    if (!didID) throw Error('DID Address Undefined');
+    const extrinsic = await authorizeLinkWithAccount(
+      api,
+      encodedAccountAddresses,
+      didID,
+      async (payload, address) => {
+        if (!injector.signer.signRaw)
+          throw Error("Extension doesn't support signRaw");
+        const result = await injector.signer.signRaw({
+          data: payload,
+          address,
+          type: 'bytes',
+        });
+        return result.signature;
+      },
+    );
+    setLinkingStep(2);
+    const signedOutputFromExtension =
+      await window.kilt.sporran.signExtrinsicWithDid(
+        extrinsic.toHex(),
+        payerAddress,
+      );
+
+    const genericExtrinsic = api.createType(
+      'Extrinsic',
+      signedOutputFromExtension.signed,
+    );
+    setLinkingStep(3);
+    const submittableExtrinsic = api.tx.did.submitDidCall(
+      genericExtrinsic.args[0],
+      genericExtrinsic.args[1],
+    );
+
+    return { payerAddress, submittableExtrinsic };
+  };
 
   const submitDidCall = async (
     payerAddress: string,
@@ -33,28 +90,27 @@ export const LinkingButton = (props: Wallet) => {
       { signer: injector.signer },
       (result) => {
         if (result.status.isFinalized) {
-          setLinking(false);
-          setSuccessMessage('The Linking was successful');
+          setLinkingStatus('success');
         }
         if (result.status.isFinalityTimeout) {
-          setLinking(false);
-          setError('The Linking was unsuccessful');
+          setLinkingStatus('error');
         }
       },
     );
   };
   const handleLinking = async () => {
-    if (linking) return;
-    setError(null);
-    setSuccessMessage(null);
+    setLinkingStep(1);
+
     if (!props.did || !props.linkingAccount || !props.payerAccount) {
       return;
     }
+    setLinkingStatus('linking');
+
     if (!isKiltDid(props.did)) {
-      setError('Not a valid Kilt Did');
+      setLinkingStatus('error');
       return;
     }
-    setLinking(true);
+
     try {
       const result = await linkDidWithAccount(
         props.linkingAccount,
@@ -62,24 +118,41 @@ export const LinkingButton = (props: Wallet) => {
         props.did,
       );
       await submitDidCall(result.payerAddress, result.submittableExtrinsic);
+      setLinkingStep(4);
     } catch {
-      setLinking(false);
-      setError('Linking was unsuccessful');
+      setLinkingStatus('error');
     }
   };
+
+  const handleSuccess = useCallback(() => {
+    if (!props.linkingAccount) throw new Error('No linking account');
+
+    const { address: linkedAccountAddress } = props.linkingAccount;
+
+    window.open(
+      `${process.env.REACT_APP_W3NID_ENDPOINT}/${linkedAccountAddress}`,
+    );
+    setLinkingStatus(undefined);
+    setLinkingStep(1);
+  }, [props.linkingAccount]);
+
   return (
     <div className={styles.container}>
       <button
-        className={linking ? styles.loader : styles.linkingBtn}
+        className={styles.linkingBtn}
         disabled={!props.did || !props.linkingAccount || !props.payerAccount}
         onClick={() => handleLinking()}
       >
         Link Did with account address
       </button>
-      {error && <label className={styles.errorLabel}>{error}</label>}
-      {successMessage && (
-        <label className={styles.successLabel}>{successMessage}</label>
-      )}
+      {
+        <LinkingModal
+          linkingStatus={linkingStatus}
+          onClose={() => setLinkingStatus(undefined)}
+          onSuccess={handleSuccess}
+          linkingStep={linkingStep}
+        />
+      }
     </div>
   );
 };
---
+++ src/Components/Popup/Popup.tsx
@@ -0,0 +1,110 @@
+import { Fragment } from 'react';
+
+import styles from './Popup.module.css';
+
+import { ReactComponent as SuccessSVG } from '../../ImageAssets/icon_ok.svg';
+
+import { ReactComponent as ErrorSVG } from '../../ImageAssets/icon_error.svg';
+
+import { ReactComponent as Spinner } from '../../ImageAssets/puff.svg';
+import { LinkingSteps } from '../LinkingButton/LinkingButton';
+
+interface ErrorContentsProps {
+  onClose: React.MouseEventHandler;
+}
+
+function ErrorContents({ onClose }: ErrorContentsProps) {
+  return (
+    <Fragment>
+      <p className={styles.modalText}>
+        Oops!
+        <br /> Click “Try Again” or reload the page or restart your browser.
+      </p>
+      <ErrorSVG className={styles.status} />
+      <button className={styles.btn} onClick={onClose}>
+        Try again
+      </button>
+    </Fragment>
+  );
+}
+
+interface SuccessContentsProps {
+  onSuccess: React.MouseEventHandler;
+  onClose: React.MouseEventHandler;
+}
+
+function SuccessContents({ onSuccess, onClose }: SuccessContentsProps) {
+  return (
+    <Fragment>
+      <p className={styles.modalText}>
+        Your account is now linked to searches of your DID/web3name on w3n.id
+      </p>
+      <SuccessSVG className={styles.status} />
+      <button className={styles.btn} onClick={onSuccess}>
+        CHECK IT NOW
+      </button>
+      <button
+        aria-label="close modal"
+        onClick={onClose}
+        className={styles.closeBtn}
+      />
+    </Fragment>
+  );
+}
+
+interface LoadingContentsProps {
+  linkingStep: LinkingSteps;
+}
+function LoadingContents({ linkingStep }: LoadingContentsProps) {
+  const linkingMessage = {
+    1: 'The extension of the account you are linking to will pop up. Enter the password of that account and click “Sign”',
+    2: 'Enter your password in Sporran and click “Sign”',
+    3: 'Sign the transaction from the chosen payer account.',
+    4: 'Your request is being processed. Please leave this tab open until the transaction is complete.',
+  };
+
+  return (
+    <Fragment>
+      <p className={styles.modalText}>{linkingMessage[linkingStep]}</p>
+      <div className={styles.spinnerContainer}>
+        <Spinner />
+        {linkingStep !== 4 && <p className={styles.step}>{linkingStep}</p>}
+      </div>
+      <button className={styles.btnDisabled}>CHECK IT NOW</button>
+    </Fragment>
+  );
+}
+
+interface Props {
+  linkingStatus?: 'linking' | 'success' | 'error';
+  onClose: React.MouseEventHandler;
+  onSuccess: React.MouseEventHandler;
+  linkingStep: LinkingSteps;
+}
+
+export function LinkingModal({
+  linkingStatus,
+  onClose,
+  onSuccess,
+  linkingStep,
+}: Props) {
+  if (!linkingStatus) {
+    return null;
+  }
+
+  return (
+    <div className={styles.modalContainer}>
+      <div className={styles.modal}>
+        <h1 className={styles.heading}>Linking process</h1>
+        {linkingStatus === 'linking' && (
+          <LoadingContents linkingStep={linkingStep} />
+        )}
+        {linkingStatus === 'success' && (
+          <SuccessContents onSuccess={onSuccess} onClose={onClose} />
+        )}
+
+        {linkingStatus === 'error' && <ErrorContents onClose={onClose} />}
+      </div>
+    </div>
+  );
+}
