30e64eb13d29ee09b8b44e7e6db4f9617838a3d5
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2022-08-10 16:32:39 +0200

    useScrollToTop

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -9,21 +9,21 @@ import { paths } from '../../Utilts/paths';
 import { Imprint } from '../Imprint/Imprint';
 import { Terms } from '../Terms/Terms';
 import { Privacy } from '../Privacy/Privacy';
-import { ScrollToTop } from '../ScrollToTop/ScrollToTop';
+import { useScrollToTop } from '../../Hooks/useScrollToTop';
 
 export const App = () => {
+  useScrollToTop();
+
   return (
     <div className={styles.body}>
       <Header />
-      <ScrollToTop>
-        <Routes>
-          <Route path={paths.home} element={<Main />} />
-          <Route path={paths.imprint} element={<Imprint />} />
-          <Route path={paths.terms} element={<Terms />} />
-          <Route path={paths.privacy} element={<Privacy />} />
-          <Route path="*" element={<Navigate to={paths.home} replace />} />
-        </Routes>
-      </ScrollToTop>
+      <Routes>
+        <Route path={paths.home} element={<Main />} />
+        <Route path={paths.imprint} element={<Imprint />} />
+        <Route path={paths.terms} element={<Terms />} />
+        <Route path={paths.privacy} element={<Privacy />} />
+        <Route path="*" element={<Navigate to={paths.home} replace />} />
+      </Routes>
       <Footer />
     </div>
   );
