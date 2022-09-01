837b5fad8749a93b6b14e8cc9195e8a8f4d11000
usmangeek <usman@galaniprojects.de>
2022-09-01 16:30:10 +0200

    SK-1266: add maintenance page

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,18 +4,28 @@ import styles from './App.module.css';
 
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
-import { Main } from '../Main/Main';
 import { paths } from '../../Utilts/paths';
 import { Imprint } from '../Imprint/Imprint';
 import { Terms } from '../Terms/Terms';
 import { Privacy } from '../Privacy/Privacy';
+import { Maintenance } from '../Maintenance/Maintenance';
+import { Main } from '../Main/Main';
 
 export const App = () => {
   return (
     <div className={styles.body}>
       <Header />
       <Routes>
-        <Route path={paths.home} element={<Main />} />
+        <Route
+          path={paths.home}
+          element={
+            process.env.REACT_APP_MAINTENANCE === 'true' ? (
+              <Maintenance />
+            ) : (
+              <Main />
+            )
+          }
+        />
         <Route path={paths.imprint} element={<Imprint />} />
         <Route path={paths.terms} element={<Terms />} />
         <Route path={paths.privacy} element={<Privacy />} />
