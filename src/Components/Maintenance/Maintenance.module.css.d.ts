07cb5a467eedd12a7a62665d82a9516da51ad60d
Muhammad Usman <94175349+usmangeek@users.noreply.github.com>
2022-09-06 17:20:40 +0200

    SK-1266: add maintenance page (#20)
    * SK-1266: add maintenance page
    
    * finalize text and style
    
    Co-authored-by: Marton Csernai <marton@kilt.io>

---
+++ src/Components/Maintenance/Maintenance.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const styles: {
+  readonly container: string;
+  readonly heading: string;
+  readonly contents: string;
+  readonly anchor: string;
+  readonly text: string;
+};
+export = styles;
