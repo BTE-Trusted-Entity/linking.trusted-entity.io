d826833c922cc3c2435a0347a6558eda918a7546
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2022-09-16 14:02:24 +0200

    SK-1273: Adjust step 2 modal text

--- src/Components/Popup/Popup.tsx
+++ src/Components/Popup/Popup.tsx
@@ -58,7 +58,7 @@ interface LoadingContentsProps {
 function LoadingContents({ linkingStep }: LoadingContentsProps) {
   const linkingMessage = {
     1: 'The extension of the account you are linking to will pop up. Enter the password of that account and click “Sign”',
-    2: 'Enter your password in Sporran and click “Sign”',
+    2: 'Select the identity in Sporran containing the DID that you copied and pasted earlier. Enter your password and click “Sign”',
     3: 'Sign the transaction from the chosen payer account.',
     4: 'Your request is being processed. Please leave this tab open until the transaction is complete.',
   };
