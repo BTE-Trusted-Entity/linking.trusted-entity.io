9d8d4ae03de63e33ab34e59d8ad2d45f3ac06299
GustavHemmelmayr <34513708+GustavHemmelmayr@users.noreply.github.com>
2022-12-15 16:19:46 +0100

    Update Imprint.tsx

--- src/Components/Imprint/Imprint.tsx
+++ src/Components/Imprint/Imprint.tsx
@@ -7,7 +7,7 @@ export const Imprint = () => {
         <h1 className={styles.heading}>Imprint</h1>
         <section className={styles.imprintSection}>
           <p className={styles.imprintLine}>
-            B.T.E. BOTLabs Trusted Entity GmbH
+            BOTLabs GmbH
           </p>
           <p className={styles.imprintLine}>Keithstraße 2-4</p>
           <p className={styles.imprintLine}>10787 Berlin, Germany</p>
@@ -18,8 +18,8 @@ export const Imprint = () => {
           <p className={styles.imprintLine}>
             Amtsgericht Charlottenburg in Berlin
           </p>
-          <p className={styles.imprintLine}>Registration Number: HRB 231219B</p>
-          <p className={styles.imprintLine}>VAT No: DE 346528612</p>
+          <p className={styles.imprintLine}>Registration Number: HRB 193450B</p>
+          <p className={styles.imprintLine}>VAT No: DE316284270</p>
           <p className={styles.imprintLine}>Managing Director: Ingo Rübe</p>
         </section>
 
