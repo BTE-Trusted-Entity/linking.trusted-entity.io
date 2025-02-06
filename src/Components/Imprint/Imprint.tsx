898631b2ffafdcbdc251712bcf893a8c39ac1002
Adel Golghalyani <ad96el@gmail.com>
2025-02-06 09:25:17 +0100

    lints

--- src/Components/Imprint/Imprint.tsx
+++ src/Components/Imprint/Imprint.tsx
@@ -7,14 +7,20 @@ export const Imprint = () => {
         <h1 className={styles.heading}>Imprint</h1>
         <section className={styles.imprintSection}>
           <p className={styles.imprintLine}>KILT Foundation</p>
-          <p className={styles.imprintLine}>Genesis Building, 5th Floor, Genesis Close,</p>
-          <p className={styles.imprintLine}>PO Box 446, Cayman Islands, KY1-1106</p>
+          <p className={styles.imprintLine}>
+            Genesis Building, 5th Floor, Genesis Close,
+          </p>
+          <p className={styles.imprintLine}>
+            PO Box 446, Cayman Islands, KY1-1106
+          </p>
         </section>
 
         <section className={styles.imprintSection}>
           <p className={styles.imprintLine}>Certificate No. 418097</p>
           <p className={styles.imprintLine}>VAT No: DE316284270</p>
-          <p className={styles.imprintLine}>Directors: Rishant Kumar, Svetoslav Boyadzhiev</p>
+          <p className={styles.imprintLine}>
+            Directors: Rishant Kumar, Svetoslav Boyadzhiev
+          </p>
         </section>
 
         <section className={styles.imprintSection}>
